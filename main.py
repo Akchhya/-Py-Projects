c0472a09175e176b5ea517aa6e92867ea5d838bc
Akchhya Singh <143519796+Akchhya@users.noreply.github.com>
2024-04-21 22:01:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 
+print("Welcome to the Band Name Generator.")
+city=input("Enter the city you grew up in :\n")
+name=input("Enter name of your pet:\n")
+print("Here's your Band name!\n")
+print(city+ " "+ name)
+print("Bye")
